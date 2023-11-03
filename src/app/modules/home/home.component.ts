4f34a64dbae1ad8ab03f69737076d4ba8f58c122
AndresMarolt <andresmmarolt@gmail.com>
2023-11-03 17:33:41 -0300

    Create quick info section in home component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  quickInfoItems = [
+    { text: 'Financiamiento flexible', icon: 'credit_score' },
+    {
+      text: 'Envío sin costo adicional',
+      icon: 'local_shipping',
+    },
+    {
+      text: 'Seguridad en tus compras en línea',
+      icon: 'lock',
+    },
+    {
+      text: 'Devoluciones sin complicaciones',
+      icon: 'assignment_return',
+    },
+  ];
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MaterialModule } from 'src/app/material/material.module';
+import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeroCarouselComponent],
   imports: [CommonModule, HomeRoutingModule, MaterialModule],
 })
 export class HomeModule {}
