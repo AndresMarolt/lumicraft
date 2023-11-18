d2bb6a3fa060a5bf9633ff162a35bbd98741d174
AndresMarolt <andresmmarolt@gmail.com>
2023-11-17 22:08:15 -0300

    Add bootstrap carousel to products with more than one image in the products grid and customize carousel buttons

---
+++ src/app/modules/products/products-carousel/products-carousel.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-products-carousel',
+  templateUrl: './products-carousel.component.html',
+  styleUrls: ['./products-carousel.component.scss'],
+})
+export class ProductsCarouselComponent {
+  carouselId!: string;
+  @Input() productImages: { id?: number; image: string }[] = [];
+
+  ngOnInit() {
+    this.carouselId = this.productImages[0].image;
+    console.log(this.carouselId);
+  }
+
+  aux(productImages: any) {
+    console.log(productImages);
+  }
+}
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -5,9 +5,10 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { FormsModule } from '@angular/forms';
+import { ProductsCarouselComponent } from './products-carousel/products-carousel.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductsCarouselComponent],
   imports: [CommonModule, FormsModule, MaterialModule, ProductsRoutingModule],
 })
 export class ProductsModule {}
