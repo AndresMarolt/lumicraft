82d64c78a94d886fd087bfa30212082d20202233
AndresMarolt <andresmmarolt@gmail.com>
2023-11-21 11:23:51 -0300

    Create product page

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
+import { MaterialModule } from 'src/app/material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ProductComponent],
-  imports: [CommonModule, ProductRoutingModule],
+  imports: [CommonModule, ProductRoutingModule, MaterialModule, SharedModule],
 })
 export class ProductModule {}
