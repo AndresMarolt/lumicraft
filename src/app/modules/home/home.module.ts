f8f70505c1370e9a5796237fc79c36b1cc28ed7a
AndresMarolt <andresmmarolt@gmail.com>
2024-02-06 12:26:43 +0100

    Design carousel in home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HomeComponent, HeroCarouselComponent],
-  imports: [CommonModule, HomeRoutingModule, MaterialModule],
+  imports: [CommonModule, HomeRoutingModule, MaterialModule, FontAwesomeModule],
 })
 export class HomeModule {}
