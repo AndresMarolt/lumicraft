6d068497e54c7722b7cf10614dfe99f597c9c9d8
AndresMarolt <andresmmarolt@gmail.com>
2024-02-07 17:25:08 +0100

    Testimonials section in home page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,6 +2,11 @@ import { Component, HostListener, OnInit, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product.interface';
 import { ProductService } from 'src/app/services/product/product.service';
+import {
+  faStar,
+  faQuoteLeft,
+  faQuoteRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +14,21 @@ import { ProductService } from 'src/app/services/product/product.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  [x: string]: any;
   private productService = inject(ProductService);
   public prominentProducts: Product[] = [];
   public sizeRow: number = 1;
   public productsChunks: Product[][] = [];
+  public testimonials: {
+    image: string;
+    name: string;
+    rating: number;
+    text: string;
+    date: Date;
+  }[] = [];
+  public faStar = faStar;
+  public faQuoteLeft = faQuoteLeft;
+  public faQuoteRight = faQuoteRight;
 
   private subscriptions: Subscription[] = [];
 
@@ -112,9 +128,57 @@ export class HomeComponent {
           }
         })
     );
+
+    this.testimonials = [
+      {
+        image:
+          'https://this-person-does-not-exist.com/img/avatar-gen112404137a1267341df49e8d88b79294.jpg',
+        name: 'María García',
+        date: new Date(),
+        rating: 5,
+        text: '¡Increíble experiencia de compra en Lumicraft! Compré un nuevo portátil y el proceso fue muy fácil y rápido. El producto llegó en perfectas condiciones y es exactamente lo que esperaba. Definitivamente volveré a comprar aquí para mis necesidades tecnológicas.',
+      },
+      {
+        image:
+          'https://this-person-does-not-exist.com/img/avatar-gen11761759b8bbd88940667c64acf58b8e.jpg',
+        name: 'Alejandro López',
+        date: new Date(),
+        rating: 5,
+        text: '¡Una tienda excepcional con una amplia gama de productos tecnológicos! Compré un smartphone y estoy completamente satisfecho con mi compra. El envío fue rápido y el servicio al cliente fue excelente. Recomiendo esta tienda a todos los amantes de la tecnología.',
+      },
+      {
+        image:
+          'https://this-person-does-not-exist.com/img/avatar-gen113ba9f8265e19f44f819faf995cb987.jpg',
+        name: 'Laura Martínez',
+        date: new Date(),
+        rating: 5,
+        text: 'Compré una nueva tablet y estoy impresionada con su rendimiento. El proceso de compra fue súper simple y, además, el equipo de atención al cliente fue muy servicial y amable. Definitivamente volveré a comprar aquí en el futuro.',
+      },
+      {
+        image:
+          'https://this-person-does-not-exist.com/img/avatar-gen11af2316128be9231935b7d217622225.jpg',
+        name: 'Javier Rodríguez',
+        date: new Date(),
+        rating: 5,
+        text: 'Compré una cámara digital y estoy muy contento con mi compra. El precio era competitivo y el producto llegó en perfectas condiciones. Además, el seguimiento del envío fue muy útil para mantenerme informado sobre el estado de mi pedido. ¡Altamente recomendado!',
+      },
+    ];
+
+    // this.testimonials.forEach((testimonial) => {
+    //   this.get.rating);
+    // });
   }
 
-  aux() {
-    console.log(this.productsChunks);
-  }
+  // getStarsArray(rating: number): number[] {
+  //   const integerPart = Math.floor(rating);
+  //   const decimalPart = rating - integerPart;
+
+  //   const starsArray = Array(integerPart).fill(1); // Fill array with 1 for full stars
+  //   if (decimalPart > 0) {
+  //     starsArray.push(2); // Add 2 for half star
+  //   }
+  //   console.log(starsArray);
+
+  //   return starsArray;
+  // }
 }
